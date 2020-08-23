d0b4ee70c5d7f115f0b99a89f5456d4d6c488ee9
L-Coetzee <g18c7090@campus.ru.ac.za>
2020-08-23 11:20:09 +0200

    Unity project files
    The project files I need help with

---
+++ Assets/ExampleScripts/SampleScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+namespace BeyondTheBasics.Gameplay
+{
+
+    public class SampleScript : MonoBehaviour
+    {
+        #region Veriable Declerations
+
+        public int numberToDisplay;
+        private int numberToMultiplyBy = 0;
+
+        #endregion
+
+        #region Private Methods
+
+        // Start is called before the first frame update
+
+        private void Start()
+        {
+            AnonymouseTypeExample();
+        }
+
+        private void SayHelloWorld()
+        {
+            var randomNumber = UnityEngine.Random.Range(0, numberToDisplay);
+            var displayText = "Hello World!";
+
+            for (var i = 0; i < randomNumber; i++)
+            {
+                Debug.Log(displayText);
+            }
+
+        }
+
+        private void MultiplyNumber(int numberToMultiply)
+        {
+            int product = numberToMultiply * numberToMultiplyBy;
+
+            Debug.Log(product);
+
+        }
+
+        private void AnonymouseTypeExample()
+        {
+            //var enemy = new { name = "Monster" , hitPoints = 100 };
+
+            var enemies = new[]
+            {
+                new {name = "Lion", hitPoints = 100},
+                new {name = "Bear", hitPoints = 200},
+                new {name = "Hippo", hitPoints = 250}
+            };
+
+            var enemyQuery =
+                from enemy in enemies
+                orderby enemy.name
+                select enemy;
+
+            foreach (var enemy in enemyQuery)
+            {
+                Debug.Log(enemy.name);
+            }
+
+        }
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/GameSceneController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void TextOutPutHandler(string text);
+
+public class GameSceneController : MonoBehaviour
+{
+    public float playerSpeed;
+    public Vector3 screenBounds;
+    public EnemyController enemyPrefab;
+
+    private HUDController hUDController;
+    private int totalPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hUDController = FindObjectOfType <HUDController>();
+        playerSpeed = 10;
+        screenBounds = GetScreenBounds();
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        WaitForSeconds wait = new WaitForSeconds(2);
+
+        while (true)
+        {
+            float horizontalPosition = Random.Range(-screenBounds.x, screenBounds.x);
+            Vector2 spawnPosition = new Vector2(horizontalPosition, screenBounds.y);
+
+            EnemyController enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+            enemy.EnemyEscaped += EnemyAtBottom;
+            enemy.EnemyKilled += EnemyKilled;
+
+            yield return wait;
+        }
+    }
+
+    void EnemyKilled(int pointValue)
+    {
+        totalPoints += pointValue;
+        hUDController.scoreText.text = totalPoints.ToString();
+    }
+
+    private void EnemyAtBottom(EnemyController enemy)
+    {
+        Destroy(enemy.gameObject);
+        Debug.Log("Enemy Escaped");
+    }
+
+    private Vector3 GetScreenBounds()
+    {
+        Camera mainCamera = Camera.main;
+        Vector3 screenVector = new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z);
+
+        return mainCamera.ScreenToWorldPoint(screenVector);
+    }
+
+    public void DestroyObject(IKillable killable)
+    {
+     
+        killable.Kill();
+    }
+
+    public void OutputText (string output)
+    {
+        Debug.LogFormat("{0} output GameSceneController", output);
+    }
+
+}
